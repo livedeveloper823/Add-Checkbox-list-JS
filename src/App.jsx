a775ff2d70b67a9ee555742ec56a95cfdb8e02f3
laihaoqiang <laihaoqiang34@gmail.com>
2024-02-26 20:28:43 +1000

    implement add function

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [addCheck, setAddCheck] = useState({
+    name: 'React',
+    list: [],
+    itemName: '',
+  })
+  const Add = () => {
+    const list = addCheck.list
+    list.push(addCheck.itemName)
+    setAddCheck({ ...addCheck, list: list })
+    setAddCheck({ ...addCheck, itemName: '' })
+  }
+  console.log(addCheck.list);
+  const Checkbox = () => {
+    return (
+      addCheck.list.map((item, index) => {
+        return (
+          <div key={index}>
+            <input type='checkbox' />
+            {item}
+            <button>Delete</button>
+          </div>
+        )
+      })
+    )
+  }
 
   return (
     <>
+      <input onChange={(e) => setAddCheck({ ...addCheck, itemName: e.target.value })} />
+      <div onClick={Add}>ADD</div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Checkbox/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
